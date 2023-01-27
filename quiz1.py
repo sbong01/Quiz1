cab2b98598df201b687cbd4a49842257afcb9ed4
Saerin Bong <123026544+sbong01@users.noreply.github.com>
2023-01-27 11:41:03 -0500

    Add files via upload

---
+++ quiz1.py
@@ -0,0 +1,14 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+#Quiz1
+#saerin Bong
+
+#problem 4
+2+(5-3)**5*27/(4*3)
+74.0
+#problem 5
+ 
+print("Be not afraid of greatness. \nSome are born great, some achieve greatness, and others have greatness thrust upon \nthem.")
+Be not afraid of greatness. 
+Some are born great, some achieve greatness, and others have greatness thrust upon 
+them.
